e5b44ab13fce350f863af6335891f3addaa84277
Odysseas Athanasiadis <o.dy.se@hotmail.com>
2023-07-24 00:39:42 +0200

    upload

---
+++ SMTPClient/SMTPClient.cs
@@ -0,0 +1,55 @@
+﻿using MailKit.Net.Imap;
+using System.Collections.Specialized;
+using static SMTPClient.SMTPHelper;
+
+namespace SMTPClient
+{
+    
+    public class SMTPClient
+    {
+        public static ImapClient EmailClient;
+        public static List<MimeKit.MimeMessage> MessageList;
+        public static List<EmailListEntity> EmailListReceived;
+        public static List<EmailListEntity> AnswerEmailList;
+
+        public static StringCollection EmailListReceivedAvailability = Settings.Default.EmailListReceivedAvailability;
+        public static StringCollection EmailListToAddresses = Settings.Default.EmailListToAddresses;
+
+        public SMTPClient() 
+        {
+            //Email Client init
+            EmailClient = new ImapClient();
+            MessageList = new List<MimeKit.MimeMessage>();
+            AnswerEmailList = new List<EmailListEntity>();
+
+            //Memory Init
+            //Last received message memory
+            string[] adressesArray;
+            foreach (var address in EmailListReceivedAvailability)
+            {
+                adressesArray = address.Split(',');
+                EmailListReceived.Add(new EmailListEntity() { obj1 = adressesArray[0].Trim(), obj2 = adressesArray[1].Trim() });
+            }
+
+            string[] emailAnswerArray;
+            foreach (var code in EmailListToAddresses)
+            {
+                emailAnswerArray = code.Split(',');
+                AnswerEmailList.Add(new EmailListEntity() { obj1 = emailAnswerArray[0].Trim(), obj2 = emailAnswerArray[1].Trim() });
+            }
+
+            if (MessageList.Count != 0)
+            {
+
+                foreach (var message in MessageList)
+                {
+                    // Iterate through received messages
+                }
+
+            }
+
+            SendEmail("This is a test email to send");
+        }
+
+    }
+}
---
+++ SMTPClient/SMTPHelper.cs
@@ -0,0 +1,172 @@
+﻿using MailKit.Security;
+using MailKit;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Specialized;
+using static SMTPClient.SMTPClient;
+using MailKit.Search;
+
+namespace SMTPClient
+{
+    public class SMTPHelper
+    {
+        public static MailMessage BackupMsg = new MailMessage();
+        public static string ClientReceiveUsername = Settings.Default.ClientReceiveUsername;
+        public static string ClientReceivePassword = Settings.Default.ClientReceivePassword;
+        public static string ClientSendUsername = Settings.Default.ClientSendUsername;
+        public static string ClientSendPassword = Settings.Default.ClientSendPassword;
+        public static string BackupClientSendUsername = Settings.Default.BackupClientSendUsername;
+        public static string BackupClientSendPassword = Settings.Default.BackupClientSendPassword;
+        public static StringCollection LogEmailList = Settings.Default.LogEmailList;
+        public static int GetEmailsHrBack = Settings.Default.GetEmailsHrBack;
+        public class EmailListEntity : IEquatable<EmailListEntity>
+        {
+            public string obj1 { get; set; }
+            public string obj2 { get; set; }
+
+            public bool Equals(EmailListEntity other)
+            {
+                if (other == null) return false;
+                return obj2.Equals(other.obj2);
+            }
+            public override string ToString()
+            {
+                return obj1;
+            }
+
+        }
+
+        public static int SMTPSend(MailMessage msg, string username, string pass)
+        {
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(username, pass),
+                    EnableSsl = true,
+
+                };
+
+                msg.From = new MailAddress(ClientSendUsername);
+                BackupMsg = msg;
+                smtpClient.Send(msg);
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Mailbox unavailable"))
+                {
+                    SMTPSend(BackupMsg, BackupClientSendUsername, BackupClientSendPassword);
+                    return 2;
+                }
+                return 0;
+            }
+
+        }
+        public static void SMTPLogSend(MailMessage msg)
+        {
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(ClientSendUsername, ClientSendPassword),
+                EnableSsl = true,
+
+            };
+
+            foreach (var email in LogEmailList)
+            {
+                msg.To.Add(email);
+            }
+
+            msg.From = new MailAddress(ClientSendUsername);
+            msg.Subject = $"Email_Parser {DateTime.Now.ToString("dd-MM-yyyy | HH:mm:ss")} | {msg.Subject}";
+            smtpClient.Send(msg);
+
+        }
+
+        public static bool ClientConnect()
+        {
+            if (!EmailClient.IsConnected)
+            {
+                EmailClient.CheckCertificateRevocation = false;
+                EmailClient.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                EmailClient.Authenticate(ClientReceiveUsername, ClientReceivePassword);
+                EmailClient.Inbox.Open(FolderAccess.ReadWrite);
+            }
+
+            if (EmailClient.IsConnected)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void GetMessageList()
+        {
+            lock (this)
+            {
+                ClientConnect();
+
+                var uids = EmailClient.Inbox.Search(SearchQuery.NotSeen.And(SearchQuery.DeliveredAfter(DateTime.Now.AddHours(-GetEmailsHrBack))));
+
+                foreach (var uid in uids)
+                {
+                    var message = EmailClient.Inbox.GetMessage(uid);
+
+                    if (EmailListReceived.Contains(new EmailListEntity { obj2 = message.From.Mailboxes.ElementAt(0).Address }))
+                    {
+                        MessageList.Add(message);
+
+                    }
+                }
+
+                EmailClient.Inbox.AddFlags(uids, MessageFlags.Seen, true);
+            }
+            EmailClient.Disconnect(true);
+        }
+
+
+        public static void SendEmail(string messageStr)
+        {
+
+            string fileName;
+            MailMessage msg = new MailMessage();
+            BackupMsg = new MailMessage();
+
+
+           msg.Subject = $"My subject";
+           fileName = "Template.html";
+
+
+            var template = File.ReadAllText($@"{AppDomain.CurrentDomain.BaseDirectory}\EmailTemplates\{fileName}");
+
+
+            //FOR PRODUCTION            
+            foreach (var contact in AnswerEmailList)
+            {
+                msg.To.Add(contact.obj1);
+
+            }
+
+            template = template
+                .Replace("{test1}", messageStr)
+                .Replace("{test2}", messageStr ?? "Test2");
+
+
+            msg.Body = template;
+            msg.IsBodyHtml = true;
+
+
+            SMTPSend(msg, ClientSendUsername, ClientSendPassword);
+
+            msg.To.Clear();
+         
+        }
+
+    }
+}
